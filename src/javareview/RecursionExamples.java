71f438b90459f06f2f33762d34c84b8fbd654c97
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-09-12 09:58:26 -0400

    School Sept.12

--- src/javareview/RecursionExamples.java
+++ src/javareview/RecursionExamples.java
@@ -18,8 +18,8 @@ public class RecursionExamples {
         int[] numbers = new int[n + 1];
         numbers[0] = 1;
         //Boundary condition fixing
-        if(n>0){
-        numbers[1] = 1;
+        if (n > 0) {
+            numbers[1] = 1;
         }
         //building the sequence
         for (int i = 2; i < n + 1; i++) {
@@ -31,15 +31,36 @@ public class RecursionExamples {
     }
 
     /**
+     *
+     * @param n
+     * @return
+     */
+    public int fibR(int n) {
+        //BASE CASE
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return fibR(n - 1) + fibR(n - 2);
+        }
+    }
+
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         RecursionExamples test = new RecursionExamples();
         int bound1 = test.fibLoop(0);
-        System.out.println("0th: "+bound1);
+        System.out.println("0th: " + bound1);
         int bound2 = test.fibLoop(1);
-        System.out.println("1th: "+bound2);
-        int norm  = test.fibLoop(5);
-        System.out.println("5th: "+norm);
+        System.out.println("1th: " + bound2);
+        int norm = test.fibLoop(5);
+        System.out.println("5th: " + norm);
+        System.out.println("-------");
+        int boundR1 = test.fibR(0);
+        System.out.println("0th: " + boundR1);
+        int boundR2 = test.fibR(1);
+        System.out.println("1th: " + boundR2);
+        int normR = test.fibLoop(5);
+        System.out.println("5th: " + normR);
     }
 }
